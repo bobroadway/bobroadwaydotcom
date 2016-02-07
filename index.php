bf48dd61ffe65eaba59dbd2a836ec8f7d385d89b
Bo Broadway <broadway.bo@gmail.com>
2016-02-06 19:20:41 -0800

    created home page

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
       $id = "index";
       include "head.php"; 
     ?>
+    <style>
+      hr {
+            margin-top: 0;
+            border-color: #333;
+            border: 0; 
+            height: 1px; 
+            background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
+        }
+    </style>
     <title>Bo Broadway - Home</title>
     </head>
-  <body onLoad="locationGlow('<?php echo $id ?>');">
+  <body onLoad="locationGlow('<?php echo $id ?>'); displayDate();">
     <?php include "navbar.php"; ?> 
     <div class="container">
-      <h1>Hello</h1>
+      <table width="90%">
+        <tr>
+          <td><h1>Greetings!</h1></td>
+          <td><span class="pull-right"><h3 id="currentdate" class="text-muted"></h3></span></td>
+        </tr>
+      </table>
+      <hr />
+      <p>Welcome to www.BoBroadway.com, the host site for my resume and project portfolio!</p>
+      <p class="text-center">Please take a look!</p>
+      <br />
+    </div>
+    <div class="container text-center">
+      <a onclick="" class="btn btn-primary btn-lg" href="resume.php">Resume</a>
+      <a onclick="" class="btn btn-primary btn-lg" href="projects/projectDemo.php">Projects</a>
     </div>
   </body>
 </html>
--- js/head.js
+++ js/head.js
@@ -3,3 +3,25 @@ function locationGlow(id) {
     label.style = "text-shadow: 0 0 4px lightblue;"
       + "color: white;";
 }
+
+function displayDate() {
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    
+    var monthArray = ["January", "February", "March", "April", "May", "June",
+                      "July", "August", "September", "October", "November", "December"];
+    var monthString = monthArray[month]
+    
+    var daySuf = "th";
+    if (day === 1 || day === 21 || day === 31) {
+        daySuf = "st";
+    } else if (day === 2 || day === 22) {
+        daySuf = "nd";
+    } else if (day === 3 || day === 23) {
+        daySuf = "rd";
+    }
+    document.getElementById("currentdate").innerHTML = monthString 
+        + " " + day + daySuf + ", " + year;
+}
