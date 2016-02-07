c1882bd60ad4e9be3a76bbf4dbeefe8b67984d13
Bo Broadway <broadway.bo@gmail.com>
2016-02-06 17:04:25 -0800

    working directory structure with navbar

---
+++ projects/projectDemo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bo Broadway - Projects</title>
+    <?php include "../bootstrap.php" ?>
+  </head>
+  <body>
+    <?php 
+      $relative = "../";
+      include "{$relative}navbar.php";
+    ?>
+    
+  </body>
+  
+  
+  
+  
+</html>
--- resume.php
+++ resume.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <?php include "bootstrap.php" ?>
     <script type="javascript/text" src="js/resume.js"></script>
     <style>
       h4 {
@@ -25,7 +18,10 @@
   </head>
   <title>Bo Broadway - Resume</title>
   <body>
-    <?php include "header.php" ?>
+    <?php 
+      $relative = "";
+      include "navbar.php";
+    ?>
     <div class="container">
       <h1><strong>BO</strong> BROADWAY</h1>
       <span class="text-muted">3117 Muir Field Rd, Madison, WI, 53719<br />
